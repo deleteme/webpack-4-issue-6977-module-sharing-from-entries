a77c390af74a258b8bdf60ea6ef77b65a03b8008
Matt Garrett <mattga@gmail.com>
2018-04-06 16:39:32 -0700

    format the config

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,43 @@
 var path = require("path");
 
 module.exports = {
-	// mode: "development" || "production",
-	entry: {
+  // mode: "development" || "production",
+  entry: {
     environment: ["babel-polyfill", "./utility2"],
     "vendor-react": ["react", "react-dom"],
     "vendor-jquery": ["jquery", "./expose-jquery"],
-		pageA: "./pageA",
-		pageB: "./pageB",
-		pageC: "./pageC"
-	},
-	optimization: {
-		splitChunks: {
-			cacheGroups: {
+    pageA: "./pageA",
+    pageB: "./pageB",
+    pageC: "./pageC"
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
         default: false,
         vendors: false,
-				environment: {
-					test: "environment",
-					chunks: "all",
-					name: "environment",
-					enforce: true
-				},
-				"vendor-react": {
-					test: "vendor-react",
-					chunks: "all",
-					name: "vendor-react",
-					enforce: true
-				},
-				"vendor-jquery": {
-					test: "vendor-jquery",
-					chunks: "all",
-					name: "vendor-jquery",
-					enforce: true
-				}
-			}
-		}
-	},
-	output: {
-		path: path.join(__dirname, "dist"),
-		filename: "[name].js"
-	}
+        environment: {
+          test: "environment",
+          chunks: "all",
+          name: "environment",
+          enforce: true
+        },
+        "vendor-react": {
+          test: "vendor-react",
+          chunks: "all",
+          name: "vendor-react",
+          enforce: true
+        },
+        "vendor-jquery": {
+          test: "vendor-jquery",
+          chunks: "all",
+          name: "vendor-jquery",
+          enforce: true
+        }
+      }
+    }
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js"
+  }
 };
